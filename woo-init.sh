ca475b5f7d49de2b5d9fe60ee8055139ace55db9
Magalie Chetrit <magaliechetrit@gmail.com>
2021-04-01 10:59:46 +0200

    added all the useful plugins

--- woo-init.sh
+++ woo-init.sh
@@ -26,8 +26,18 @@ if [ -d "$HASWP" ]; then
     echo ${DIR} 
     echo "${YELLOW}This folder has WordPress installed. Continuiing with installing plugins and themes..."
     wp plugin install --activate https://github.com/afragen/github-updater/archive/master.zip
-    echo "Github updater has been installed... next..."
-
+    echo "${YELLOW}Github updater has been installed... next..."
+    wp plugin --activate install advanced-custom-fields
+    echo "${YELLOW}ACF Has been installed... next..."
+    wp plugin --activate install woocommerce
+    echo "${YELLOW}Woo has been installed... next, let's start installing Storefront..."
+    wp theme install storefront --activate
+    echo "${YELLOW}Storefront is installed and activated... next, let's start uninstalling some things..."
+    wp plugin --deactivate uninstall hello
+    echo "${YELLOW}Hello has been uninstalled"
+    wp plugin --deactivate uninstall akismet
+    echo "${YELLOW}Akismet has been uninstalled"
+    wp theme --deactivate uninstall twentynineteen twentytwenty twentytwentyone
 else
     echo "There is no WordPress installed. I will install it"
     exit 1
